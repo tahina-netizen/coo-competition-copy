7b179715a9a2e7edcd73f57d5c09bc7caea040bc
zo tahina <zratefiarivony@gmail.com>
2022-01-05 10:37:30 +0100

    refact: rewrite MapUtil

--- src/main/java/etu/simonzo/competition/util/MapUtil.java
+++ src/main/java/etu/simonzo/competition/util/MapUtil.java
@@ -1,8 +1,6 @@
 package etu.simonzo.competition.util;
 
 import java.util.ArrayList ;
-import java.util.Collections ;
-import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List ;
 import java.util.Map;
@@ -10,20 +8,12 @@ import java.util.Map.Entry;
 
 public class MapUtil {
     public static <K, V extends Comparable<? super V>>
+    
     Map<K, V> sortByDescendingValue(Map<K, V> map) {
-        List<Entry<K, V>> sortedEntries = new ArrayList<Entry<K, V>>(map.entrySet());
-
-        Collections.sort(sortedEntries, new Comparator<Entry<K, V>>() {
-                @Override
-                public int compare(Entry<K, V> e1, Entry<K, V> e2) {
-                    return e2.getValue().compareTo(e1.getValue());
-                }
-            });
-
+        List<Entry<K, V>> sortedEntries = new ArrayList<>(map.entrySet());        
+        sortedEntries.sort(Entry.comparingByValue((V v1, V v2) -> v2.compareTo(v1)));
         Map<K, V> result = new LinkedHashMap<>();
-        for (Entry<K, V> entry : sortedEntries) {
-            result.put(entry.getKey(), entry.getValue());
-        }
+        sortedEntries.forEach((Entry<K, V> e) -> result.put(e.getKey(),e.getValue()));
         return result;
     }
 }
